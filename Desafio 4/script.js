51d7e670576831c59ce0f077ada0a330315ffa22
Ivo Augusto <ivomudo@gmail.com>
2022-08-19 20:50:44 -0300

    correção erro portugues

--- Desafio 4/script.js
+++ Desafio 4/script.js
@@ -1,10 +1,10 @@
 //Desafio 4: Desenhar triangulo
 
-const triangleBase = Number(prompt('Qual o tamanho da base do triâsngulo?'))
+const triangleBase = Number(prompt('Qual o tamanho da base do triângulo?'))
 
 const triangleChar = prompt('De qual caractere o tringulo será feito?', '*')
 
-for (let i = 0; i <= triangleBase; i++) {
+for (let i = 0; i < triangleBase; i++) {
     let row = '';
     for (let j = 0; j <= i; j++) {
         row += triangleChar
